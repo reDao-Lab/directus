41b16b602ac76ce139c6beb23258f7f6d7f5f78b
TechRedao <tech@redao.org>
2023-09-26 09:30:56 +0700

    update web3 endpoints

--- extensions/endpoints/web3/src/index.ts
+++ extensions/endpoints/web3/src/index.ts
@@ -1,5 +1,11 @@
 import { defineEndpoint } from '@directus/extensions-sdk';
 
-export default defineEndpoint((router) => {
-	router.get('/', (_req, res) => res.send('Hello, World!'));
-});
+export default defineEndpoint(
+	{
+		id: 'web3',
+		handler: (router) =>
+		{
+			router.get('/', (_req, res) => res.send('Hello, World!'))
+		}
+	}
+)
